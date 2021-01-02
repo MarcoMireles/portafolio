26239b2667112c33678a41501ba7c7e52e8944ac
Marco Mireles <marcomirelesvilla@gmail.com>
2021-01-01 23:52:17 -0600

    Diseño actualizado

--- footer.php
+++ footer.php
@@ -14,9 +14,25 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 
+<?php
+  if(!is_front_page() ) :
+?>
+<footer class="mt-8">
+  <div class="container mx-auto px-4">
+    <div class="block pt-8 pb-4">
+      <p class="text-center">Hecho con Wordpress y mucho ❤</p>
+      <p class="text-center">Todos los derechos e izquierdos reservados</p>
+      <p class="text-center">2021©</p>
+    </div>
+  </div>
+</footer>
+<?php endif; ?>
+
 <?php wp_footer(); ?>
 
 </body>
 
 </html>
+
+
 
