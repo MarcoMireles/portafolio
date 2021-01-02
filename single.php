883afff8dfbc93216d8232816d8c5f4b0a045707
Marco Mireles <marcomirelesvilla@gmail.com>
2021-01-01 23:51:20 -0600

    Diseño actualizado

--- single.php
+++ single.php
@@ -13,22 +13,20 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 
-<section>
-  <div class="container mx-auto lg:py-10 py-5">
-    <div class="grid grid-cols-1 lg:grid-cols-2 px-6  py-4">
-      <div >
-        <h2 class="text-2xl text-mm font-black lg:text-3xl mb-2">¿Cómo utilizar vegas slideshow?</h2>
-        <div class="text-mm font-normal text-base lg:text-2xl lg:pr-4">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores at autem consequatur dicta dolore esse, exercitationem laboriosam libero molestiae numquam obcaecati odio odit praesentium repellendus sequi similique, sunt.</p>
+<section class="lg:my-16 my-8">
+  <div class="container mx-auto lg:py-10 py-5 bg-white">
+    <div class="grid grid-cols-1 px-6  py-4">
+      <div class="block">
+        <div class="block mb-4">
+          <img class="w-full" src="" alt="Lorem ipsum">
+        </div>
+        <div class="block mb-4">
+          <h1 class="text-primary text-3xl lg:text-5xl font-bold">Lorem ipsum</h1>
+        </div>
+        <div class="block mb-4 text-lg contenedor-post">
+	          dolor sit amet, consectetur adipisicing elit. Doloremque eos eveniet excepturi exercitationem illum inventore maiores nostrum omnis, optio porro praesentium quam, quis quo quod sequi sint soluta velit vitae.
         </div>
       </div>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/xq3MuF-M1A0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
-    <div class="contenido my-4 lg:my-8 text-mm leading-8 w-full lg:w-2/3 mx-auto text-lg ">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores at autem consequatur dicta dolore esse, exercitationem laboriosam libero molestiae numquam obcaecati odio odit praesentium repellendus sequi similique, sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolore earum eligendi eum in magni maiores, molestiae, molestias necessitatibus, nemo neque officiis provident quaerat quasi ratione recusandae repellat voluptatem voluptates. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam animi aperiam architecto commodi dolores esse maiores maxime molestiae nihil numquam quae quod repellat, rerum soluta, voluptate? Amet expedita nam quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid beatae cum dolores eius eligendi enim eveniet exercitationem, expedita hic ipsa obcaecati omnis provident quia repudiandae, sint soluta voluptates! Quasi.</p>
-      <h2>Here is a title</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores at autem consequatur dicta dolore esse, exercitationem laboriosam libero molestiae numquam obcaecati odio odit praesentium repellendus sequi similique, sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolore earum eligendi eum in magni maiores, molestiae, molestias necessitatibus, nemo neque officiis provident quaerat quasi ratione recusandae repellat voluptatem voluptates. </p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid asperiores at autem consequatur dicta dolore esse, exercitationem laboriosam libero molestiae numquam obcaecati odio odit praesentium repellendus sequi similique, sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolore earum eligendi eum in magni maiores, molestiae, molestias necessitatibus, nemo neque officiis provident quaerat quasi ratione recusandae repellat voluptatem voluptates. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam animi aperiam architecto commodi dolores esse maiores maxime molestiae nihil numquam quae quod repellat, rerum soluta, voluptate? Amet expedita nam quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid beatae cum dolores eius eligendi enim eveniet exercitationem, expedita hic ipsa obcaecati omnis provident quia repudiandae, sint soluta voluptates! Quasi.</p>
     </div>
   </div>
 </section>
